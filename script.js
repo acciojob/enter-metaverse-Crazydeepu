8594663b8577f2edce0f64fc7a305bf2f3b27249
Anudeep Reddy <95900205+Crazydeepu@users.noreply.github.com>
2023-09-18 22:49:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let status = document.getElementById("status");
+let btn = document.getElementById("enterBtn");
+
+function name() {
+	btn.onclick = status.innerHtml = "Entered Metaverse";
+}
